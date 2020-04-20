f0ea5008621775a8c77d4df48643d42b5de0a9e3
Blankgen <63858859+Blankgen@users.noreply.github.com>
2020-04-20 12:47:18 +0200

    Update capture.js

--- js/capture.js
+++ js/capture.js
@@ -8,7 +8,7 @@ document.getElementById("button").addEventListener("click", function() {
     html2canvas(container, {scale:1000/containerWidth}).then(function(canvas){
         var a = document.createElement('a');
         a.href = canvas.toDataURL("blank.png");
-        a.download = 'myfile.png';
+        a.download = 'blank.png';
         a.click();
     })
 })
